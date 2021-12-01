d4840a55486bf000968b19425d9003d3344ffeea
Victor Kropp <victor.kropp@jetbrains.com>
2021-12-01 22:27:44 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asInts().zipWithNext().count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asInts().windowed(3).map { it.sum() }.zipWithNext().count { (a, b) -> b > a }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -7,6 +7,8 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
+fun List<String>.asInts() = map { it.toInt() }
+
 /**
  * Converts string to md5 hash.
  */
